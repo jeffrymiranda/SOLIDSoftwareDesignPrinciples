38042f08fbd0dfcfaa22eb13358120ea863f2665
Jeffry Miranda Toruno <jeffryuna@gmail.com>
2021-07-05 22:53:09 -0600

    first commit

---
+++ Core/SOLIDSoftwareDesignPrinciples.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core
+{
+    class SOLIDSoftwareDesignPrinciples
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SOLID Software Design Principles!");
+        }
+    }
+}
